69dfe93cf4c592f8ff126e81468163ca67d02272
Katie Humburg <pookakat@gmail.com>
2019-06-16 20:13:17 -0400

    make stuff show up when accessing api/friends

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var friends = require('../data/friends.js');
 
 var bodyParser = require("body-parser");
 
@@ -7,11 +8,11 @@ var bodyParser = require("body-parser");
 module.exports = function(app) {
     app.use(bodyParser.urlencoded({ extended: false }));
     app.get('/api/friends', (req, res, next) => {
-
-    })
+        res.send(friends);
+    });
 
     app.post('/api/friends', (req,res,next) =>{
         var newUser = req.body;
-        console.log(newUser);
-    })
-}
+        module.exports = newUser;
+        });
+};
